a28711c16de1ec8914673aa62cd9c52fd7f986f6
33base <setikhonenko@gmail.com>
2024-10-20 17:47:46 +0300

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from app.routers import healthcheck
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(healthcheck.router)
 
